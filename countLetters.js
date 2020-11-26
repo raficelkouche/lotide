de4dd220ee6d0a84d584ea88048d51d27cda5e6e
Rafic El Kouche <raficelkouche@hotmail.com>
2020-11-26 00:16:26 +0000

    added a new function that returns the frequency of all letters of a string

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const assertEqual = function (actual, expected) {
+  console.log((actual === expected) ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` :
+    `🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = function(string) {
+  string = string.split(" ").join(""); //remove the spaces from the string
+  const result = {};
+
+  for (let char of string){
+    result[char] ? result[char]++ : result[char] = 1;
+  }
+  return result;
+}
+//Testing
+let testA = countLetters("LHL");
+let testB = countLetters("lighthouse in the house");
+
+assertEqual(testA["L"], 2);
+assertEqual(testB["h"], 4);
