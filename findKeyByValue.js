11adf3c26423786134a85c153d9844191e3db56a
Rafic El Kouche <raficelkouche@hotmail.com>
2020-11-26 01:32:47 +0000

    added a function that finds a key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,22 @@
+const assertEqual = function (actual, expected) {
+  console.log((actual === expected) ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` :
+    `🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function(object, value) {
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
