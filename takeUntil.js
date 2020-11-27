7182ef7bb209069530d3d2052e6c9b28df882f43
Rafic El Kouche <raficelkouche@hotmail.com>
2020-11-26 19:39:56 -0500

    added a function that rejects elements that pass a certain test

---
+++ takeUntil.js
@@ -0,0 +1,33 @@
+const takeUntil = function(array, callback){
+  const result = [];
+  let i = 0;
+  while (!callback(array[i])){
+    result.push(array[i++])
+  }
+  return result;
+}
+const assertArraysEqual = function (arrayA, arrayB) {
+  console.log((eqArrays(arrayA, arrayB)) ? `✅✅✅ Assertion Passed: ${arrayA} === ${arrayB}` :
+    `🛑🛑🛑  Assertion Failed: ${arrayA} !== ${arrayB}`);
+};
+const eqArrays = function (arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] !== arrayB[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1,2,5,7,2]);
+
+console.log('---------');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
