3b3e37076cac3ba37b866a9f00c059022a641768
Rafic El Kouche <raficelkouche@hotmail.com>
2020-11-30 15:42:57 -0500

    added a tester for the middle function

--- middle.js
+++ middle.js
@@ -1,23 +1,7 @@
-const assertArraysEqual = function(arrayA, arrayB) {
-  console.log((eqArrays(arrayA, arrayB)) ? `✅✅✅ Assertion Passed: ${arrayA} === ${arrayB}` :
-    `🛑🛑🛑  Assertion Failed: ${arrayA} !== ${arrayB}`);
-};
-
-const eqArrays = function(arrayA, arrayB) {
-  if (arrayA.length !== arrayB.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayA.length; i++) {
-    if (arrayA[i] !== arrayB[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 /* [1,2,3,4,5,6] => indx = 3, mid = [3,4] => [indx-1,indx+1]*/
 const middle = function(array){
   let indexOfMiddle;
-  
+
   if (array.length > 2) {
     indexOfMiddle = Math.floor(array.length / 2);
   }
@@ -33,6 +17,5 @@ const middle = function(array){
   }
 }
 
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1]), []);
+module.exports = middle;
+
